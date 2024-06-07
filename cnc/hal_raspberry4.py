b5e5b839a5e3169ef8b964e4f15be6450a7ab020
ACR_i7 <dzhackerse7@gmail.com>
2024-06-07 22:22:34 +0100

    try to implement pi4

--- cnc/hal_raspberry4.py
+++ cnc/hal_raspberry4.py
@@ -104,108 +104,54 @@ def calibrate(x, y, z):
 
 # noinspection PyUnusedLocal
 def move(generator):
-    """ Move head to specified position.
-    :param generator: PulseGenerator object.
-    """
-    delta = generator.delta()
-    ix = iy = iz = ie = 0
-    lx, ly, lz, le = None, None, None, None
-    dx, dy, dz, de = 0, 0, 0, 0
-    mx, my, mz, me = 0, 0, 0, 0
-    cx, cy, cz, ce = 0, 0, 0, 0
-    direction_x, direction_y, direction_z, direction_e = 1, 1, 1, 1
+        # Activer les steppers
+    GPIO.output(STEPPERS_ENABLE_PIN, GPIO.LOW)
+    
+    prev = 0
     st = time.time()
-    direction_found = False
+    k = 0
+    k0 = 0
+
     for direction, tx, ty, tz, te in generator:
-        if direction:
-            direction_found = True
-            direction_x, direction_y, direction_z, direction_e = tx, ty, tz, te
-            if STEPPER_INVERTED_X:
-                direction_x = -direction_x
-            if STEPPER_INVERTED_Y:
-                direction_y = -direction_y
-            if STEPPER_INVERTED_Z:
-                direction_z = -direction_z
-            if STEPPER_INVERTED_E:
-                direction_e = -direction_e
-            if isinstance(generator, PulseGeneratorLinear):
-                assert ((direction_x < 0 and delta.x < 0)
-                        or (direction_x > 0 and delta.x > 0) or delta.x == 0)
-                assert ((direction_y < 0 and delta.y < 0)
-                        or (direction_y > 0 and delta.y > 0) or delta.y == 0)
-                assert ((direction_z < 0 and delta.z < 0)
-                        or (direction_z > 0 and delta.z > 0) or delta.z == 0)
-                assert ((direction_e < 0 and delta.e < 0)
-                        or (direction_e > 0 and delta.e > 0) or delta.e == 0)
+        if direction:  # Configurer les directions
+            if tx is not None:
+                GPIO.output(STEPPER_DIR_PIN_X, GPIO.HIGH if tx > 0 else GPIO.LOW)
+            if ty is not None:
+                GPIO.output(STEPPER_DIR_PIN_Y, GPIO.HIGH if ty > 0 else GPIO.LOW)
+            if tz is not None:
+                GPIO.output(STEPPER_DIR_PIN_Z, GPIO.HIGH if tz > 0 else GPIO.LOW)
+            if te is not None:
+                GPIO.output(STEPPER_DIR_PIN_E, GPIO.HIGH if te > 0 else GPIO.LOW)
             continue
-        if tx is not None:
-            if tx > mx:
-                mx = tx
-            tx = int(round(tx * 1000000))
-            ix += direction_x
-            cx += 1
-            if lx is not None:
-                dx = tx - lx
-                assert dx > 0, "negative or zero time delta detected for x"
-            lx = tx
-        else:
-            dx = None
-        if ty is not None:
-            if ty > my:
-                my = ty
-            ty = int(round(ty * 1000000))
-            iy += direction_y
-            cy += 1
-            if ly is not None:
-                dy = ty - ly
-                assert dy > 0, "negative or zero time delta detected for y"
-            ly = ty
-        else:
-            dy = None
-        if tz is not None:
-            if tz > mz:
-                mz = tz
-            tz = int(round(tz * 1000000))
-            iz += direction_z
-            cz += 1
-            if lz is not None:
-                dz = tz - lz
-                assert dz > 0, "negative or zero time delta detected for z"
-            lz = tz
-        else:
-            dz = None
-        if te is not None:
-            if te > me:
-                me = te
-            te = int(round(te * 1000000))
-            ie += direction_e
-            ce += 1
-            if le is not None:
-                de = te - le
-                assert de > 0, "negative or zero time delta detected for e"
-            le = te
-        else:
-            de = None
-        # very verbose, uncomment on demand
-        # logging.debug("Iteration {} is {} {} {} {}".
-        #               format(max(ix, iy, iz, ie), tx, ty, tz, te))
-        f = list(x for x in (tx, ty, tz, te) if x is not None)
-        assert f.count(f[0]) == len(f), "fast forwarded pulse detected"
+        
+        pins = []
+        m = None
+        for i, pin in zip((tx, ty, tz, te), (STEP_PIN_MASK_X, STEP_PIN_MASK_Y, STEP_PIN_MASK_Z, STEP_PIN_MASK_E)):
+            if i is not None:
+                pins.append(pin)
+                if m is None or i < m:
+                    m = i
+        
+        k = int(round(m * US_IN_SECONDS))
+        if k - prev > 0:
+            time.sleep((k - prev) * US_IN_SECONDS)
+        
+        for pin in pins:
+            GPIO.output(pin, GPIO.HIGH)
+        
+        time.sleep(STEPPER_PULSE_LENGTH_US * US_IN_SECONDS)
+        
+        for pin in pins:
+            GPIO.output(pin, GPIO.LOW)
+        
+        prev = k + STEPPER_PULSE_LENGTH_US
+    
     pt = time.time()
-    assert direction_found, "direction not found"
-    assert round(ix / STEPPER_PULSES_PER_MM_X, 10) == delta.x,\
-        "x wrong number of pulses"
-    assert round(iy / STEPPER_PULSES_PER_MM_Y, 10) == delta.y,\
-        "y wrong number of pulses"
-    assert round(iz / STEPPER_PULSES_PER_MM_Z, 10) == delta.z, \
-        "z wrong number of pulses"
-    assert round(ie / STEPPER_PULSES_PER_MM_E, 10) == delta.e, \
-        "e wrong number of pulses"
-    assert max(mx, my, mz, me) <= generator.total_time_s(), \
-        "interpolation time or pulses wrong"
-    logging.debug("Moved {}, {}, {}, {} iterations".format(ix, iy, iz, ie))
-    logging.info("prepared in " + str(round(pt - st, 2)) + "s, estimated "
+    logging.info("préparé en " + str(round(pt - st, 2)) + "s, estimé en "
                  + str(round(generator.total_time_s(), 2)) + "s")
+    
+    # Désactiver les steppers
+    GPIO.output(STEPPERS_ENABLE_PIN, GPIO.HIGH)
 
 
 def join():
